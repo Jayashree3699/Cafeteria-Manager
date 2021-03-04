6b65fe403ff8b1f86f9fa330f70d2477a6abad4e
Jayashree3699 <17euec054@skcet.ac.in>
2021-03-04 11:47:44 +0530

    heroku root1

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
-    has_many :order_items
-    belongs_to :user
 
+    belongs_to :user
+    has_many :order_items
     def self.not_delivered
         orders = Order.joins(:user).select("users.name as user_name, users.email as user_email, orders.id as order_id, orders.date as order_date").where(orders: {order_delivered: false})
         orders.collect{|order| [order.user_name , order.user_email , order.order_id , order.order_date]}
